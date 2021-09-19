23fad836a2daff3cd168d032b655dab2c390921f
Sreekumar M <srkmololath@gmail.com>
2021-09-19 15:50:13 +0530

    first commit thru vs code

---
+++ DistinctLongestSubArray.py
@@ -0,0 +1,8 @@
+# Given an array of elements, return the length of the longest sub array where all its elements are distinct.
+# For Example, given the array [5,1,3,5,2,3,4,1]
+# return 5 as the longest sub array of disticnt elements is [5,2,3,4,1]
+
+input_array = [5,1,3,5,2,3,4,1]
+
+for i in input_array:
+    print(i)
